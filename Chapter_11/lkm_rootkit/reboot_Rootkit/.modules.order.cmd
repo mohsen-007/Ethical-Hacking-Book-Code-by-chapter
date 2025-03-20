7228a349d690129d3640bf908ec0f9ee0327be4f
mohsen-007 <54136591+mohsen-007@users.noreply.github.com>
2025-03-20 20:00:43 +0330

    Add files via upload

---
+++ Chapter_11/lkm_rootkit/reboot_Rootkit/.modules.order.cmd
@@ -0,0 +1,1 @@
+savedcmd_/home/noob/Desktop/ethical-hacking/Chapter_11/xxx/c/modules.order := {   echo /home/noob/Desktop/ethical-hacking/Chapter_11/xxx/c/reboot_blocker.o; :; } > /home/noob/Desktop/ethical-hacking/Chapter_11/xxx/c/modules.order
---
+++ Chapter_11/lkm_rootkit/reboot_Rootkit/modules.order
@@ -0,0 +1,1 @@
+/home/noob/Desktop/ethical-hacking/Chapter_11/xxx/c/reboot_blocker.o
